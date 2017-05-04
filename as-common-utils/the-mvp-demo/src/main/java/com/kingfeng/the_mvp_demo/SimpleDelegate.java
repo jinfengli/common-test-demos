fd41f92fa0aa793d3de3e2e25c443b5540a0cb8b
Jinfeng Li <lijinfeng.ljf@gmail.com>
2017-05-04 18:30:02 +0800

    test TheMVP

---
+++ as-common-utils/the-mvp-demo/src/main/java/com/kingfeng/the_mvp_demo/SimpleDelegate.java
@@ -0,0 +1,12 @@
+package com.kingfeng.the_mvp_demo;
+
+/**
+ * TODO:
+ *
+ * @author lijf
+ * @version 1.0.0
+ * @date $today
+ * @copyright (c) wonhigh.cn All rights reserved.
+ */
+public class SimpleDelegate {
+}
---
+++ as-common-utils/the-mvp-demo/src/main/java/com/kingfeng/the_mvp_demo/base/view/AppDelegate.java
@@ -0,0 +1,12 @@
+package com.kingfeng.the_mvp_demo.base.view;
+
+/**
+ * TODO:
+ *
+ * @author lijf
+ * @version 1.0.0
+ * @date $today
+ * @copyright (c) wonhigh.cn All rights reserved.
+ */
+public class AppDelegate {
+}
